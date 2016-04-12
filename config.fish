ee8cd903611a1f446a26dafc4f6e60f0fe83fd86
gozes <gozes17@gmail.com>
2016-04-12 05:22:21 -0400

    disable greeting

--- config.fish
+++ config.fish
@@ -3,3 +3,4 @@ set -x PATH $PATH $HOME/.go/bin $HOME/Code/go/bin
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
 source $fisher_home/config.fish
+set fish_greeting ""
