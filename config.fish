f0708626b4fdeeeb26a317e4219f7bad10e92751
gozes <gozes17@gmail.com>
2016-04-18 08:02:17 -0400

    fix typo in config

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
-set -x $GOPATH $HOME/.go $HOME/Code/go
+set -x GOPATH $HOME/.go $HOME/Code/go
 set -x PATH $PATH $HOME/.go/bin $HOME/Code/go/bin
 set fisher_home ~/.local/share/fisherman
 set fisher_config ~/.config/fisherman
